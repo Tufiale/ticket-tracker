44e54ea76c388b33d6083964ab6b6724f4869619
Tufiale <tufiale@gmail.com>
2023-08-16 14:51:24 +0100

    Cannot decrement below 0

--- src/Components/Employees/EmployeeCard.tsx
+++ src/Components/Employees/EmployeeCard.tsx
@@ -1,4 +1,5 @@
 import "../../Components/Employees/EmployeeCard.scss";
+import { useState } from "react";
 
 type teamProp = {
   id?: number;
@@ -7,10 +8,27 @@ type teamProp = {
 };
 
 const EmployeeCard = ({ name, role }: teamProp) => {
+  const [ticket, setTicket] = useState(0);
+
+  const ticketCountPlus = () => {
+    setTicket(ticket + 1);
+  };
+
+  const ticketCountMinus = () => {
+    if (ticket === 0) {
+      return 0;
+    } else {
+      setTicket(ticket - 1);
+    }
+  };
+
   return (
     <div className="card">
       <div className="employee__container">
         <h2>Name:{name}</h2>
+        <button onClick={ticketCountPlus}>+</button>
+        <p>Ticket: {ticket} </p>
+        <button onClick={ticketCountMinus}>-</button>
         <h2>Role:{role}</h2>
       </div>
     </div>
